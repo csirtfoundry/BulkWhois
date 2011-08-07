32da742ece55f9f27b351718e0c0bfeaafe6ac23
chorsley <cmrhorsley@gmail.com>
2011-08-08 01:05:01 +1000

    Set download URL to http version - cert failed

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='BulkWhois',
-    version='0.2.0',
+    version='0.2.1',
     author='CSIRT Foundry / Chris Horsley',
     author_email='chris.horsley@csirtfoundry.com',
     packages=['bulkwhois'],
     scripts=[],
     url='http://pypi.python.org/pypi/BulkWhois/',
-    download_url='https://github.com/csirtfoundry/BulkWhois/tarball/master',
+    download_url='http://github.com/csirtfoundry/BulkWhois/tarball/master',
     license='LICENSE.txt',
     description='Interfaces for popular bulk WHOIS servers',
     long_description=open('README.txt').read(),
