b0ac5ae340d582fed67e75214350cf2702529c5a
chorsley <cmrhorsley@gmail.com>
2011-09-12 18:00:05 +1000

    Fixed typo in domain name for test wrapper

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='BulkWhois',
-    version='0.2.1',
+    version='0.2.2',
     author='CSIRT Foundry / Chris Horsley',
     author_email='chris.horsley@csirtfoundry.com',
     packages=['bulkwhois'],
